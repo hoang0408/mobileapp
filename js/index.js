40500a2b7b775b6655aa76de948e24c32a537a6f
hoang0408 <120462722+hoang0408@users.noreply.github.com>
2023-01-31 21:52:15 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 window.addEventListener("DOMContentLoaded",
     function() {
         // ベージ本体が読み込まれたタイミングで実行するコード
-        const item = document.querySelectorAll(".item");
+        const item = document.querySelectorAll(".item01, .item02");
         item.forEach(function(element, index){
             //0.2s毎にずれて表示
             setTimeout(function () {
